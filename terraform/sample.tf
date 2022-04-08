db4a9bdf2c6e94326ba8cf79301216e5c2715882
vishal-kapse <vishal.kapse@cldcvr.com>
2022-04-08 20:45:59 +0530

    test pr

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.15.0"
     }
   }
 }
-
+#
 provider "google" {
   # Configuration options
 }
